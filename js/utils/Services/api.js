a6ffc8f890f50fea9c0351681d93a9172c34d10f
kyh196201 <53055051+kyh196201@users.noreply.github.com>
2020-04-09 22:24:05 +0900

    Update api.js

--- js/utils/Services/api.js
+++ js/utils/Services/api.js
@@ -1,26 +1,40 @@
 import { setBase } from "../Services/functions.js";
 
 const CORS = "https://cors-anywhere.herokuapp.com";
-const END_POINT = "http://apis.data.go.kr/1360000/VilageFcstInfoService";
+const END_POINT = "http://apis.data.go.kr/1360000";
+const SERVICE = {
+  VILAGE: "VilageFcstInfoService",
+  MID: "MidFcstInfoService",
+};
 const API_KEY =
   "FrXPuaL0ls5tHLK%2F2oPRuCoxI6cYNM93hPWcE6Ta0%2B4gZ6BNNCbirWsBEpoJd6tCieQmHFMdYco1fAxaVe0nyg%3D%3D";
+
+const G_API_KEY = "AIzaSyAXE7WzKYeqyDL5V78FdrnriYQ7ukR04Bw";
 
 //초단기실황 api
 //입력받을 데이터, baseDate, baseTime, 좌표
 
-const getNowWeather = async ({ date, gridXY }) => {
+const getNowWeather = async (params) => {
   //Date를 통해 basetime, basedate계산
+  const date = new Date();
   const mode = "current";
   const baseData = setBase({ mode, date });
+  const NUMOFROWS = 50;
   try {
     const response = await fetch(
-      `${CORS}/${END_POINT}/getUltraSrtNcst?serviceKey=${API_KEY}&numOfRows=30&pageNo=1&dataType=JSON&base_date=${baseData.baseDate}&base_time=${baseData.baseTime}&nx=${gridXY.x}&ny=${gridXY.y}`
+      `${CORS}/${END_POINT}/${SERVICE.VILAGE}/getUltraSrtFcst?serviceKey=${API_KEY}&numOfRows=${NUMOFROWS}&pageNo=1&dataType=JSON&base_date=${baseData.baseDate}&base_time=${baseData.baseTime}&nx=${params.x}&ny=${params.y}`
     );
 
     const resStatus = response.status;
     switch (resStatus) {
       case 200:
-        return await response.json();
+        const result = await response.json();
+        if (result.response.header.resultCode !== "00") {
+          const err = new Error();
+          err.message = `${result.response.header.resultCode},${result.response.header.resultMsg}`;
+          throw err;
+        }
+        return result;
         break;
       case 400:
         throw new Error(`Error Occured = ${resStatus}`);
@@ -39,46 +53,52 @@ const getNowWeather = async ({ date, gridXY }) => {
 };
 
 //동네예보 API
-//isReload를 통해 새로고침인지 최초 어플 실행인지 구분하여 가져오는
 //데이터의 개수 및 baseTime을 조정
-const getVilWeather = async ({ date, gridXY, isReload }) => {
+const getVilWeather = async (params) => {
   let baseData = null;
-  let numOfRows = 0;
+  let NUMOFROWS = 0;
 
-  //새로고침 시에는 현재 시간을 기준으로 데이터를 가져온다,
-  //numOfRows를 10, SKY만을 파싱
-  if (isReload) {
-    const mode = "vilage";
-    baseData = setBase({ mode, date });
-    numOfRows = 10;
-  }
-  // 최초 어플리케이션이 실행될 경우 또는 매시 새벽 2시에 전날 23시 데이터를 기준으로
-  // 최저/ 최고 온도 , SKY 지수를 가져온다.
-  //numOfRows를 50
-  else {
-    date.setDate(date.getDate() - 1);
+  const date = new Date();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+
+  if (params.isInit) {
+    //새벽 2시 이전
+    if (hours < 2) {
+      date.setDate(date.getDate() - 1);
+    }
     const todayDate = date.getDate();
     const month = date.getMonth() + 1;
     const year = date.getFullYear();
 
-    numOfRows = 50;
+    NUMOFROWS = 200;
     baseData = {
       baseDate: `${year}${month < 10 ? `0${month}` : month}${
         todayDate < 10 ? `0${todayDate}` : todayDate
       }`,
-      baseTime: "2300"
+      baseTime: "0200",
     };
+  } else {
+    const mode = "vilage";
+    baseData = setBase({ mode, date });
+    NUMOFROWS = 200;
   }
 
   try {
     const response = await fetch(
-      `${CORS}/${END_POINT}/getVilageFcst?serviceKey=${API_KEY}&numOfRows=${numOfRows}&pageNo=1&dataType=JSON&base_date=${baseData.baseDate}&base_time=${baseData.baseTime}&nx=${gridXY.x}&ny=${gridXY.y}`
+      `${CORS}/${END_POINT}/${SERVICE.VILAGE}/getVilageFcst?serviceKey=${API_KEY}&numOfRows=${NUMOFROWS}&pageNo=1&dataType=JSON&base_date=${baseData.baseDate}&base_time=${baseData.baseTime}&nx=${params.x}&ny=${params.y}`
     );
 
     const resStatus = response.status;
     switch (resStatus) {
       case 200:
-        return await response.json();
+        const result = await response.json();
+        if (result.response.header.resultCode !== "00") {
+          const err = new Error();
+          err.message = `${result.response.header.resultCode},${result.response.header.resultMsg}`;
+          throw err;
+        }
+        return result;
         break;
       case 400:
         throw new Error(`Error Occured = ${resStatus}`);
@@ -95,4 +115,26 @@ const getVilWeather = async ({ date, gridXY, isReload }) => {
   }
 };
 
-export { getNowWeather, getVilWeather };
+const displayLocation = (params) => {
+  const URL = `${CORS}/https://maps.googleapis.com/maps/api/geocode/json?latlng=${params.lat},${params.lng}&sensor=true&key=AIzaSyAXE7WzKYeqyDL5V78FdrnriYQ7ukR04Bw`;
+  return fetch(URL).then((res) => {
+    if (res.ok) {
+      return res.json();
+    } else {
+      throw new Error("Reponse Error");
+    }
+  });
+};
+
+const getCity = ({ type, wideCode, cityCode }) => {
+  const URL = `${CORS}/https://www.weather.go.kr/w/rest/zone/dong.do?type=${type}&wideCode=${wideCode}&cityCode=${cityCode}`;
+  return fetch(URL).then((res) => {
+    if (res.ok) {
+      return res.json();
+    } else {
+      throw new Error("Response Error");
+    }
+  });
+};
+
+export { getNowWeather, getVilWeather, getCity, displayLocation };
